bbd718a10e2a96bea0250c55216b1b8ceac97213
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-05-29 13:24:32 +0200

    Fonction fire faite (mais pas testée)

--- gestionParties.js
+++ gestionParties.js
@@ -28,11 +28,11 @@ function createBlankGame() {
 					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,],
 				],
 				ships: [
-					[[1, 1, 1, 1, 1], [0, 0], 1],  // Porte avion : 5 cases, position de départ, direction du navire
-					[[1, 1, 1, 1], [0, 0], 1],     // Croiseur
-					[[1, 1, 1], [0, 0], 1],        // Contre torpilleur 1
-					[[1, 1, 1], [0, 0], 1],        // Contre torpilleur 2
-					[[1, 1], [0, 0], 1],           // Torpilleur
+					[5, [0, 0], 1],  // Porte avion : 5 cases, position de départ, direction du navire
+					[4, [0, 0], 1],  // Croiseur
+					[3, [0, 0], 1],  // Contre torpilleur 1
+					[2, [0, 0], 1],  // Contre torpilleur 2
+					[1, [0, 0], 1],  // Torpilleur
 				]
 			},
 			{
@@ -49,11 +49,11 @@ function createBlankGame() {
 					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,],
 				],
 				ships: [
-					[[1, 1, 1, 1, 1], [0, 0], 1],  // Porte avion : 5 cases, position de départ, direction du navire
-					[[1, 1, 1, 1], [0, 0], 1],     // Croiseur
-					[[1, 1, 1], [0, 0], 1],        // Contre torpilleur 1
-					[[1, 1, 1], [0, 0], 1],        // Contre torpilleur 2
-					[[1, 1], [0, 0], 1],           // Torpilleur
+					[5, [0, 0], 1],  // Porte avion : 5 cases, position de départ, direction du navire
+					[4, [0, 0], 1],  // Croiseur
+					[3, [0, 0], 1],  // Contre torpilleur 1
+					[2, [0, 0], 1],  // Contre torpilleur 2
+					[1, [0, 0], 1],  // Torpilleur
 				]
 			}
 		]
@@ -75,11 +75,11 @@ Exemple d'état d'un des joueurs
 			[-1, -1, -1, -1, -1, -1, -1,  3,  3,  3,],
 		],
 		ships: [
-			[[1, 1, 0, 1, 1], [1, 1], 1],  // Porte avion : a perdu sa case centrale
-			[[1, 1, 0, 0],    [2, 8], 0],  // Croiseur    : a perdu toute son avant (ou arrière ?)
-			[[0, 0, 0],       [9, 0], 1],  // Contre torpilleur 1 : coulé
-			[[1, 1, 1],       [9, 7], 1],  // Contre torpilleur 2 : en pleine santé
-			[[1, 1],          [5, 3], 0],  // Torpilleur  : en pleine santé
+			[4, // Porte avion : a perdu sa case centrale
+			2,  // Croiseur    : a perdu toute son avant (ou arrière ?)
+			0,  // Contre torpilleur 1 : coulé
+			3,  // Contre torpilleur 2 : en pleine santé
+			2], // Torpilleur  : en pleine santé
 		]
 */
 
@@ -155,18 +155,63 @@ async function getGameData(gameId) {
 }
 
 /**
+ * TODO: write the function
+ */
+async function setGameData(state, gameId) {
+
+}
+
+/**
  * Updates the game identified by gameId
  * @param request The information associated with the request, of form {"x": int, "y": int, "player": 1 or 2}
  * @returns The http code and return info
  */
 async function fire (request, gameId) {
+	let state = await getGameData (gameId);
+
 	let {x, y, player} = request;
-	if (!(0 <= x <= 9) || !(0 <= y <= 9) || !(1 <= player <= 2)) {  // Si la case n'est pas dans la grille, ou le joueur n'est pas valide
+	if (!(0 <= x <= 9)  // Si la case n'est pas valide
+		|| !(0 <= y <= 9) // Ou si le joueur ne participe pas
+		|| (player != state.p0token && player != state.p1token)) {
 		return 400, {}  // Bad request
 	}
 
-	state = await getGameData (gameId);
-	return 200, {"kill": false};
+	let player_num = -1;  // On trouve l'indice du joueur
+	if (player == state.p0token) {
+		player_num = 0
+	} else if (player == state.p1token) {
+		player_num = 1
+	} else {
+		return 400, {}  // Bad request
+	}
+
+	// Si ce n'est pas à son tour
+	if (state.currentPlayer != player_num) {
+		return 400, {}  // Bad request
+	}
+
+	// Là, on va enfin pouvoir jouer
+	state.currentPlayer = 1 - player_num
+	let navire_touche = state.boards[player_num].board[x][y]
+
+	// Si on ne touche pas de navire :
+	if (navire_touche == -1) {
+		setGameData (state, gameId);
+		return 200, {"kill": false, "hit": false};  // Plouf
+	}
+
+	// Sinon :
+	// la case est marquée comme vide
+	state.boards[player_num].board[x][y] = -1
+	state.boards[player_num].ships[navire_touche] -= 1
+
+	setGameData (state, gameId);
+	if (state.boards[player_num].ships[navire_touche][0] == 0) {
+		return 200, {"kill": true, "hit": true}
+	}
+	else {
+		return 200, {"kill": false, "hit": true}
+	}
 }
 
 
